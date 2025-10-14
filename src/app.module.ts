d9031b29a8c93bb75e02c50c54b7156097d4c9bf
Ton <everton.xavier.efxv@gmail.com>
2025-10-14 16:23:57 -0300

    entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,34 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VeterinarioModule } from './veterinario/veterinario.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [VeterinarioModule],
+  // imports: [VeterinarioModule],
+  imports: [
+   ConfigModule.forRoot({
+     isGlobal: true,
+   }),
+   TypeOrmModule.forRootAsync({
+     imports:[ConfigModule],
+     useFactory:(configService: ConfigService) => ({
+       type:'mysql',
+       host:configService.get<string>('MYSQL_DB_HOST'),
+       port:configService.get<number>('MYQSL_DB_PORT'),
+       username:configService.get<string>('MYSQL_DB_USERNAME'),
+       password:configService.get<string>('MYSQL_DB_PASSWORD'),
+       database:configService.get<string>('MYSQL_DB_DATABASE'),
+
+
+       entities:[__dirname + '/**/*.entity{.ts,.js}'],
+       synchronize:true,
+     }),
+     inject:[ConfigService],
+   }),
+   VeterinarioModule,
+ ],
+
   controllers: [AppController],
   providers: [AppService],
 })
--- src/veterinario/entities/veterinario.entity.ts
+++ src/veterinario/entities/veterinario.entity.ts
@@ -1,1 +1,37 @@
-export class Veterinario {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Veterinario {
+    @PrimaryGeneratedColumn()
+    id_veterinario: number;
+
+    @Column({
+       length:10
+   })
+   crmv: string;
+
+   @Column({
+    length:100
+   })
+   nome_veterinario: string;
+
+   @Column({
+    length: 50
+   })
+   especialidade: string;
+
+   @Column({
+    length: 100
+   })
+   endereco: string;
+
+   @Column({
+    length: 15
+   })
+   telefone: string;
+
+   @Column({
+    length: 50
+   })
+   email: string;
+}
